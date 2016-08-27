22c37832ff6d7be3067cdd841fc933a9a5a31c13
Kiran Challa <kichalla@microsoft.com>
2016-08-26 17:28:47 -0700

    Added Mvc.Formatters sample

---
+++ samples/Mvc.Formatters/Mvc.Formatters/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mvc.Formatters.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        // POST home/echo
+        [HttpPost]
+        public string Echo([FromBody]string value)
+        {
+            return value;
+        }
+    }
+}
---
+++ samples/Mvc.Formatters/Mvc.Formatters/TextPlainInputFormatter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace Mvc.Formatters
+{
+    public class TextPlainInputFormatter : TextInputFormatter
+    {
+        public TextPlainInputFormatter()
+        {
+            SupportedMediaTypes.Add("text/plain");
+            SupportedEncodings.Add(Encoding.UTF8);
+        }
+
+        protected override bool CanReadType(Type type)
+        {
+            return type == typeof(string);
+        }
+
+        public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context, Encoding encoding)
+        {
+            var request = context.HttpContext.Request;
+            string data = null;
+            using (var streamReader = context.ReaderFactory(request.Body, encoding))
+            {
+                data = await streamReader.ReadToEndAsync();
+            }
+            return InputFormatterResult.Success(data);
+        }
+    }
+}
